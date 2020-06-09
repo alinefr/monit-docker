172adf5b6cc902562d59164783af9a3513da45e6
Aline Freitas <aline.freitas@escale.com.br>
2020-06-09 08:34:00 -0300

    fix: it was restauring original monitrc.
    We need to check if new one already exist instead

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f /etc/monitrc ]; then
-  mv /etc/monitrc /etc/monit/ \
+if [ ! -f /etc/monit/monitrc ]; then
+  cp /etc/monitrc /etc/monit/ \
   && sed -i 's/^#.*include.*\/etc\/monit.d\/\*/include \/etc\/monit\/monit.d\/*/' /etc/monit/monitrc
 fi
 
